0adcda8c348937730d7fb20fe57149b6909d394c
Nacho López <nacholopezcuadrado@gmail.com>
2022-08-19 11:37:36 +0200

    Final para despliegue

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.use(express.json())
 app.use("/api/astronomy/landings",landingRoutes)
 app.use("/api/astronomy/neas", neaRoutes)
 app.use("/api/astronomy/users", userRoutes)
-app.listen(process.env.ENV_PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${process.env.ENV_PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
 })
