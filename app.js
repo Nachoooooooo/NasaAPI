ff6203fd4489caba216ccbbd8bfcaffc9f74e337
Nacho <nnnnnachooooo@gmail.com>
2022-10-08 21:56:46 +0200

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv")
 const express = require("express")
+var cors = require('cors');
 const landingRoutes = require("./routes/landing.routes")
 const neaRoutes = require("./routes/nea.routes")
 const userRoutes = require ("./routes/user.routes")
@@ -10,6 +11,7 @@ const app = express()
 const connection = require("./utils/db")
 
 connection()
+app.use(cors());
 app.use(express.json())
 app.use("/api/astronomy/landings",landingRoutes)
 app.use("/api/astronomy/neas", neaRoutes)
